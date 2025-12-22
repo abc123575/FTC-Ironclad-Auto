71130d6f8e92c398fa9aca0c7d9b1f766efed46b
bo.lindsey <bo.lindsey750@gmail.com>
2025-12-21 20:08:46 -0600

    Bo3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleAutoPathing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleAutoPathing.java
@@ -30,7 +30,7 @@ public class SampleAutoPathing extends OpMode{
 
     private final Pose startPoseL = new Pose(20.403022670025187, 123.14357682619648, Math.toRadians(138));
     private final Pose shootPoseL = new Pose(51.37291689,91.76526531, Math.toRadians(138));
-    private final Pose ballPoseL = new Pose(51.37291689, 63.13659359190556, Math.toRadians((138))
+    private final Pose ballPoseL = new Pose(51.37291689, 63.13659359190556, Math.toRadians(138));
 
     // declare variable name object
 
