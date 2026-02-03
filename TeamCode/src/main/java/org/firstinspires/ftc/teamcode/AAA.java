c91b795bf55c78c6668ed84f1f89aa56e34cebe9
abc123575 <toxic0909bc@gmail.com>
2026-02-03 11:00:29 -0600

    Anthony 6

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AAA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AAA.java
@@ -25,7 +25,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
-
+// hello
 @Autonomous(name = "AAA", group = "Autonomous")
 @Configurable // Panels
 public class AAA extends OpMode {
